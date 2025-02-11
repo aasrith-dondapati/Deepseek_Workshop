544205d6417d69d8d885d41e0f76a184aba6fe8c
Aasrith Dondapati <aasrith.au@gmail.com>
2025-02-11 20:24:11 +1100

    googlecolab

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Deepseek_Workshop
+
+Deepseek integrations using open router
+
+interface:
+
+1. Hugging face
+2. Groq
+3. Openrouter.ai
--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 # pip install -U langchain-ollama
 # pip install streamlit
 
-
-
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
+
+import streamlit as st
+
+st.title("AD Chatbot using Deepseek-R1")
 
 template = """question: {question} Answer: Generate the answer step by step"""
 
@@ -15,7 +17,7 @@ model = OllamaLLM(model="deepseek-r1")
 
 chain = prompt | model
 
-question = input("Enter your question here: ")
+question = st.text_input("Enter your question here: ")
 
 if question:
     try:
@@ -23,7 +25,9 @@ if question:
 
         response = chain.invoke({"question": question})
 
-        print("Response:", response)
+        #print("Response:", response)
+        st.write("Response:", response)
 
     except Exception as e:
-        print(f"Error: {e}")
+        #print(f"Error: {e}")
+        st.write(f"Error: {e}")
